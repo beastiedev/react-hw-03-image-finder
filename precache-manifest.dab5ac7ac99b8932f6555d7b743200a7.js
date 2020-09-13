f4a38b21a3cbbf2bcba9c981d8917ea722cfd8d1
Pavlo Bessmertnyy <pavlo.bessmertnyy@globallogic.com>
2020-09-13 10:48:40 +0300

    Updates

---
+++ precache-manifest.dab5ac7ac99b8932f6555d7b743200a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a758925f00d83282a447628236c50923",
+    "url": "/react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "2f9ba549284c0ebf9153",
+    "url": "/react-hw-03-image-finder/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "196c5c3848f8b25fd48f",
+    "url": "/react-hw-03-image-finder/static/js/2.90689b02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hw-03-image-finder/static/js/2.90689b02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f9ba549284c0ebf9153",
+    "url": "/react-hw-03-image-finder/static/js/main.60349fdd.chunk.js"
+  },
+  {
+    "revision": "063253bfbd13644b51dd",
+    "url": "/react-hw-03-image-finder/static/js/runtime-main.f4f19f46.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-hw-03-image-finder/static/media/logo.5d5d9eef.svg"
+  }
+]);
